4963d4b6dd0fec25748b44e454eb549867b652a4
Katya <kittyserenakat@gmail.com>
2023-02-15 21:05:02 -0500

    Add refine nextjs router for auth pages

--- pages/[[...refine]].tsx
+++ pages/[[...refine]].tsx
@@ -1,29 +1,56 @@
 import { GetServerSideProps } from "next";
 import {
-  NextRouteComponent /*, handleRefineParams */,
+  NextRouteComponent, 
+  handleRefineParams, 
+  checkAuthentication
 } from "@pankod/refine-nextjs-router";
-import { checkAuthentication } from "@pankod/refine-nextjs-router";
+import { dataProvider } from "@pankod/refine-supabase";
 
 import { authProvider } from "src/authProvider";
+import { supabaseClient } from "src/utility";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // const { resource, action, id } = handleRefineParams(context.params?.refine);
+export const getServerSideProps: GetServerSideProps<
+  { initialData?: unknown }
+> = async (context) => {
+  const { resource, action, id } = handleRefineParams(context.params?.refine);
 
-  const { isAuthenticated, ...authenticationValues } =
+  const { isAuthenticated, ...props } =
     await checkAuthentication(authProvider, context);
 
-  if (!isAuthenticated) {
-    return {
-      ...authenticationValues,
-      props: {
-        ...("props" in authenticationValues ? authenticationValues.props : {}),
-      },
-    };
-  }
-
-  return {
-    props: {},
-  };
+    if (!isAuthenticated) {
+      return props;
+    }
+  
+      try {
+          if (resource && action === "show" && id) {
+              const data = await dataProvider(supabaseClient).getOne({
+                  resource: resource.slice(resource.lastIndexOf("/") + 1),
+                  id,
+              });
+  
+              return {
+                  props: {
+                      initialData: data,
+                  },
+              };
+          } else if (resource && !action && !id) {
+              const data = await dataProvider(supabaseClient).getList({
+                  resource: resource.slice(resource.lastIndexOf("/") + 1),
+              });
+  
+              return {
+                  props: {
+                      initialData: data,
+                  },
+              };
+          }
+      } catch (error) {
+          return { props: {} };
+      }
+  
+      return {
+          props: {},
+      };
 };
 
 export default NextRouteComponent;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,62 +1,86 @@
 import React from "react";
 import { AppProps } from "next/app";
+
 import { Refine } from "@pankod/refine-core";
 import {
-  AuthPage,
-  notificationProvider,
-  RefineSnackbarProvider,
-  CssBaseline,
-  GlobalStyles,
-  ReadyPage,
-  ErrorComponent,
+	// AuthPage,
+	notificationProvider,
+	RefineSnackbarProvider,
+	CssBaseline,
+	GlobalStyles,
+	ReadyPage,
+	ErrorComponent,
 } from "@pankod/refine-mui";
 import routerProvider from "@pankod/refine-nextjs-router";
 import { dataProvider } from "@pankod/refine-supabase";
-import { MuiInferencer } from "@pankod/refine-inferencer/mui";
 import { RefineKbarProvider } from "@pankod/refine-kbar";
+import { MuiInferencer } from "@pankod/refine-inferencer/mui";
+
+import { AuthPage } from "../src/components/pages/auth";
 import { authProvider } from "src/authProvider";
 import { supabaseClient } from "src/utility";
+
 import { ColorModeContextProvider } from "@contexts";
 import { Title, Sider, Layout, Header } from "@components/layout";
 import { OffLayoutArea } from "@components/offLayoutArea";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <ColorModeContextProvider>
-      <CssBaseline />
-      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-      <RefineSnackbarProvider>
-        <RefineKbarProvider>
-          <Refine
-            routerProvider={routerProvider}
-            dataProvider={dataProvider(supabaseClient)}
+	return (
+		<ColorModeContextProvider>
+			<CssBaseline />
+			<GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+			<RefineSnackbarProvider>
+				<RefineKbarProvider>
+					<Refine
+						routerProvider={{
+							...routerProvider,
+							routes: [
+								{
+									path: "/register",
+									element: (<AuthPage type="register" />),
+								},
+								{
+									path: "/forgot-password",
+									element: (<AuthPage type="forgotPassword" />),
+								},
+								{
+									path: "/update-password",
+									element: <AuthPage type="updatePassword" />,
+								},
+							],
+						}}
+            LoginPage={() => (<AuthPage type="login"/>)}
             authProvider={authProvider}
-            LoginPage={AuthPage}
-            notificationProvider={notificationProvider}
-            ReadyPage={ReadyPage}
-            catchAll={<ErrorComponent />}
-            resources={[
-              {
-                name: "posts",
-                list: MuiInferencer,
-                edit: MuiInferencer,
-                show: MuiInferencer,
-                create: MuiInferencer,
-                canDelete: true,
-              },
-            ]}
-            Title={Title}
-            Sider={Sider}
-            Layout={Layout}
-            Header={Header}
-            OffLayoutArea={OffLayoutArea}
-          >
-            <Component {...pageProps} />
-          </Refine>
-        </RefineKbarProvider>
-      </RefineSnackbarProvider>
-    </ColorModeContextProvider>
-  );
+						dataProvider={dataProvider(supabaseClient)}
+						notificationProvider={notificationProvider}
+						ReadyPage={ReadyPage}
+						catchAll={<ErrorComponent />}
+						resources={[
+							{
+								name: "posts",
+								list: MuiInferencer,
+								edit: MuiInferencer,
+								show: MuiInferencer,
+								create: MuiInferencer,
+								canDelete: true,
+							},
+						]}
+						Title={Title}
+						Sider={Sider}
+						Layout={Layout}
+						Header={Header}
+						OffLayoutArea={OffLayoutArea}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+            }}
+					>
+						<Component {...pageProps} />
+					</Refine>
+				</RefineKbarProvider>
+			</RefineSnackbarProvider>
+		</ColorModeContextProvider>
+	);
 }
 
 export default MyApp;
